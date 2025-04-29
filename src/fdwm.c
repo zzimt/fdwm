735c592b59a5e6c96854d3f4b59d2cfed5ae82bd
2IMT <daniil_t04@mail.ru>
2025-04-29 22:31:20 +0300

    Don't fully override layout symbol in monocle mode

--- src/fdwm.c
+++ src/fdwm.c
@@ -982,12 +982,15 @@ void maprequest(XEvent* e) {
 void monocle(Monitor* m) {
     unsigned int n = 0;
     Client* c;
+    char oldltsymbol[sizeof m->ltsymbol] = { 0 };
+    memcpy(oldltsymbol, m->ltsymbol, sizeof m->ltsymbol);
 
     for (c = m->clients; c; c = c->next)
         if (ISVISIBLE(c))
             n++;
-    if (n > 0) /* override layout symbol */
-        snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
+    if (n > 0) { /* override layout symbol */
+        snprintf(m->ltsymbol, sizeof m->ltsymbol, "%s(%d)", oldltsymbol, n);
+    }
     for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
         resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
 }
