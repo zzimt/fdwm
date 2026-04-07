d6b191364f8f4d5f9048ba997b604f13fe00eddd
2IMT <daniil_t04@mail.ru>
2026-04-07 11:02:26 +0300

    Fix getatomprop regression from heap overflow fix

--- src/fdwm.c
+++ src/fdwm.c
@@ -725,7 +725,7 @@ void focusstack(const Arg* arg) {
 
 Atom getatomprop(Client* c, Atom prop) {
     int di;
-    unsigned long dl;
+    unsigned long nitems, dl;
     unsigned char* p = NULL;
     Atom da, atom = None;
 
@@ -736,9 +736,9 @@ Atom getatomprop(Client* c, Atom prop) {
         req = xatom[XembedInfo];
 
     if (XGetWindowProperty(dpy, c->win, prop, 0L, sizeof atom, False, req, &da,
-            &di, &dl, &dl, &p) == Success &&
+            &di, &nitems, &dl, &p) == Success &&
         p) {
-        if (dl > 0)
+        if (nitems > 0)
             atom = *(Atom*)p;
         if (da == xatom[XembedInfo] && dl == 2)
             atom = ((Atom*)p)[1];
